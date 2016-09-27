8298976e7710ab6376752f0c73df0ae675508327
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-09-27 23:05:09 +0100

    Install Bugsnag for Laravel
    After updating your local repository from upstream, do the following:
    
    1. Open command line and navigate to the source folder
        e.g cd C:\xampp\htdocs\src-website\source
    
    2. Run this command to install Bugsnag for bug tracking
        $ composer require "bugsnag/bugsnag-laravel:^2.0"
    
    3. Set the Bugsnag API key in the .env file
        BUGSNAG_API_KEY=[uNiqUekEY]
    
        Please check the contact the admin for the [uNiqUekEY].
    
    THANKS

--- source/app/Providers/AppServiceProvider.php
+++ source/app/Providers/AppServiceProvider.php
@@ -34,6 +34,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //Bugsnag
+        $this->app->alias('bugsnag.logger', \Illuminate\Contracts\Logging\Log::class);
+        $this->app->alias('bugsnag.logger', \Psr\Log\LoggerInterface::class);
     }
 }
--- source/config/app.php
+++ source/config/app.php
@@ -168,8 +168,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Bugsnag' => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
     ],
 
 ];
