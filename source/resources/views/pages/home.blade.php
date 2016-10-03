a27b95a833a8f347fb733fa806647df8c04c72c2
Chukwuemeka Nwobodo <jcnwobodo@users.noreply.github.com>
2016-10-03 20:14:10 +0100

    Setup Stub-Template for Homepage (#37)
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Set placeholder for homepage markup
    
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Implemented the homepage
    
    Yet to be completed though.
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Refactor: Move the content of Kofacts /index.php to /source/resources/views/pages/home.blade.php
    
    * Set placeholder for homepage markup

--- source/resources/views/pages/home.blade.php
+++ source/resources/views/pages/home.blade.php
@@ -5,5 +5,11 @@
  * Date:    9/29/2016
  * Time:    2:21 PM
  **/
- 
- 
+?>
+@extends('layouts.default')
+@section('content')
+    <!--replace the div below with your markup Sir, thanks-->
+    <div class="mdl-typography--text-center">
+        <h1>Homepage Markup</h1>
+    </div>
+@endsection
