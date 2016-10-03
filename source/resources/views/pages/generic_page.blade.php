cc1f6f1317dd01d0e1294ed0a0ce87fc419d7fb5
Victor Alagwu <victoralagwu@gmail.com>
2016-10-03 17:42:37 +0100

    Update generic_page.blade.php

--- source/resources/views/pages/generic_page.blade.php
+++ source/resources/views/pages/generic_page.blade.php
@@ -2,8 +2,48 @@
 /**
  * Project: srcng.com
  * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
- * Date:    9/29/2016
- * Time:    3:00 PM
+ * Date:    10/3/2016
+ * Time:    2:29 AM
  **/
- 
- 
+?>
+@extends('layouts.default')
+
+<div class="mdl-grid">
+	<div class="mdl-cell--12-col">
+		<b>About |<span> Page</span></b>
+	</div>
+</div>
+<div class="mdl-grid">
+		<div class="mdl-cell--12-col">
+		<img src="image/cover_image.jpg" class="responsive-img" style="width: 100%; height: 250px;" alt="cover_image">
+		</div>
+</div>
+
+<div class="mdl-grid">
+		<!--Post-->
+	    <div class="mdl-cell mdl-cell--7-col">
+	    	<br/>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+	    </div>
+	    <!--Post-->
+
+	    <div class="mdl-cell--2-col">
+        </div>
+        <!--Column for Related Pages-->
+	    <div class="mdl-cell--3-col">
+	    <br/>
+		    <b style="font-family: 'Arial'; font-size: 20px;">Related Pages</b>
+			    <ul>
+					<li>Page One</li>
+					<li>Page Two</li>
+					<li>Page Three</li>
+			    </ul>
+    	</div>
+    	<!--Column for Related Pages-->
+</div>
