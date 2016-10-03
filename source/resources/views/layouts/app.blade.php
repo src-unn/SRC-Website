e28597e360bab03f7ec903cef1a377ea8d91e9be
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-03 01:51:01 +0100

    MDL-Setup: Integrate MDL in base application layout (layouts/app.blade.php)
    Add icon.png for site icon

--- source/resources/views/layouts/app.blade.php
+++ source/resources/views/layouts/app.blade.php
@@ -3,35 +3,38 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="{{$meta['description'] or config('app.name')}}">
-    <meta name="author" content="{{$meta['author'] or config('app.name')}}">
-
-    <title>{{$meta['title'] or 'Awesome Title ...'}} | ZeeSaa Books</title>
-
-    <!-- Styles -->
-    <link rel="icon" href="{{ url('/favicon.ico') }}">
-    <link href="{{url('/css/material-icons.css')}}" rel="stylesheet">
-    <link href="{{url('/css/materialize.css')}}" rel="stylesheet">
-
-    <!-- Scripts -->
     <script>
         window.Laravel = <?= json_encode( [ 'csrfToken' => csrf_token() ] ) ?>;
     </script>
+
+    <title>{{$meta['title'] or config('app.name')}}</title>
+    <meta name="description" content="{{$meta['description'] or config('app.name')}}">
+    <meta name="author" content="{{$meta['author'] or config('app.name')}}">
+
+    <!-- Styles -->
+    <link rel="icon" href="{{ url('icon.png') }}">
+    @if(config('app.env')=='production')
+        <link href="{{url('roboto.font.gstatic.css')}}" rel="stylesheet">
+    @else
+        <link href="{{url('roboto.font.local.css')}}" rel="stylesheet">
+    @endif
+    <link href="{{url('css/material.icons.css')}}" rel="stylesheet">
+    <link href="{{url('css/material.min.css')}}" rel="stylesheet">
+    <link href="{{url('css/srcng.styles.css')}}" rel="stylesheet">
+
+    @yield('extra_heads')
+
 </head>
 <body>
-
-    @yield('header')
-
-    @yield('content')
-
-    @yield('footer')
+    @yield('body_markup')
 
     <!-- Scripts -->
-    <script type="text/javascript" src="{{url('/js/jquery.min.js')}}"></script>
-    <script type="text/javascript" src="{{url('js/materialize.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('js/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('js/material.min.js')}}"></script>
+    @yield('extra_scripts')
 </body>
 </html>
